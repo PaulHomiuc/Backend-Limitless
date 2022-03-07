e39952ff64ffac588a0021a5b65c319abb1c463e
claudiu-reut <claudiu.reut@student.usv.ro>
2022-03-08 00:05:45 +0200

    populated db with 2 users from server

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import mongoose from "mongoose";
 import cors from "cors";
 import postRoutes from "./routes/panou.js";
 import dotenv from "dotenv";
+import User from "./models/signUp.js";
 dotenv.config();
 const app = express();
 app.use("/posts", postRoutes);
@@ -11,8 +12,33 @@ app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 
 app.use(cors());
-app.post("/register", (req, res) => {
-  console.log(req.body);
+app.use(express.json());
+app.post("/register", async (req, res) => {
+  try {
+    //console.log(req.body);
+    const user = await User.create({
+      firstName: req.body.fname,
+      lastName: req.body.lname,
+      email: req.body.email,
+      password: req.body.password,
+      role: req.body.role,
+      gender: req.body.gender,
+      birthDate: req.body.date,
+      nationality: req.body.national,
+    });
+
+    res.json({status: "ok"});
+  } catch (err) {
+    res.json({status: "error", err: "duplicate email"});
+  }
+});
+app.post("/login", async (req, res) => {
+  const user = await User.FindOne({
+    email: req.body.email,
+    password: req.body.password,
+  });
+  if (user) return res.json({status: "ok", user: true});
+  else return res.json({status: "error", user: false});
 });
 
 const CONNECTION_URL = process.env.DATABASE_ACCESS;
--- server/models/signUp.js
+++ server/models/signUp.js
@@ -1,42 +1,45 @@
 import mongoose from "mongoose";
+//const mong = require("mongoose");
+const signUpTemplate = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
 
-const signUpTemplate = new mongoose.Schema({
-  firstName: {
-    type: String,
-    required: true,
+    lastName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      required: true,
+      enum: ["admin", "officeadmin", "employee"],
+    },
+    gender: {
+      type: String,
+      required: true,
+      enum: ["Female", "Male", "Other"],
+    },
+    birthDate: {
+      type: Date,
+      required: false,
+    },
+    nationality: {
+      type: String,
+      required: false,
+    },
   },
-
-  lastName: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  role: {
-    type: String,
-    required: true,
-    enum: ["admin", "officeadmin", "employee"],
-  },
-  gender: {
-    type: String,
-    required: true,
-    enum: ["Female", "Male", "Other"],
-  },
-  birthDate: {
-    type: Date,
-    required: false,
-  },
-  nationality: {
-    type: String,
-    required: false,
-  },
-});
-//module.exports = mongoose.model("mytable", signUpTemplate);
+  {collection: "user-data"}
+);
+//module.exports = mong.model("mytable", signUpTemplate);
 export default mongoose.model("mytable", signUpTemplate);
