5abf991e9d35c4576c10e96a5c723c4f2f774e65
claudiu-reut <claudiu.reut@student.usv.ro>
2022-03-13 23:41:25 +0200

    users table api

--- server/index.js
+++ server/index.js
@@ -7,6 +7,7 @@ import dotenv from "dotenv";
 import User from "./models/signUp.js";
 import Office from "./models/desk-model.js";
 import jwt, {decode} from "jsonwebtoken";
+
 dotenv.config();
 const app = express();
 app.use("/posts", postRoutes);
@@ -63,6 +64,34 @@ app.get("/api/offices", async (req, res) => {
     console.log(err.message);
   }
 });
+app.get("/api/users", async (req, res) => {
+  try {
+    const users = await User.find();
+
+    res.status(200).json(users);
+    // console.log(offices);
+  } catch (err) {
+    res.status(404).json({message: err.message});
+
+    console.log(err.message);
+  }
+});
+app.post("/api/delete", async (req, res) => {
+  try {
+    console.log(req.body);
+    const id = req.params.id;
+    console.log(id);
+    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
+
+    console.log(id);
+    const users = await User.findByIdAndRemove({_id: id});
+    res.json({message: "Post deleted successfully."});
+  } catch (err) {
+    res.status(404).json({message: err.message});
+
+    console.log(err.message);
+  }
+});
 app.post("/api/login", async (req, res) => {
   const user = await User.findOne({
     email: req.body.email,
@@ -81,6 +110,35 @@ app.post("/api/login", async (req, res) => {
   } else {
     return res.json({status: "error", user: false});
   }
+});
+
+app.get("/users/:id", (req, res) => {
+  const id = req.params.id;
+  console.log(req.body);
+  try {
+    User.findById(id, (err, user) => {
+      res.json(user);
+    });
+  } catch (err) {
+    console.log(err);
+  }
+});
+app.post("/update/:id", (req, res) => {
+  const id = req.params.id;
+  User.findById(id, (err, user) => {
+    if (!user) {
+      res.status(404).send("Todo not found");
+    } else {
+      user.lastName = req.body.lastName;
+
+      user
+        .save()
+        .then((user) => {
+          res.json(user);
+        })
+        .catch((err) => res.status(500).send(err.message));
+    }
+  });
 });
 app.get("/api/admin", async (req, res) => {
   const token = req.headers["x-access-token"];
